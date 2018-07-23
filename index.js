032834afdc99687bee30adca328800506036085c
Shamaera Campbell <stcampbell.0@gmail.com>
2018-07-23 18:33:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase())
   {return "I can\'t hear you!"}
-}
-
-function sayHiToGrandma(string) { 
-  if (string.toUpperCase())
+  else if (string.toUpperCase())
   {return "YES INDEED!"}
 }
+
+
